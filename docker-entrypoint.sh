caac0cf333418041160e41f1a9a7c96a8c74b6dd
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-11-27 16:35:49 +0530

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -jar  /Signal-Server/service/target/TextSecureServer-2.55.jar messagedb migrate /Signal-Server/service/config/Signal.yml
-java -jar  /Signal-Server/service/target/TextSecureServer-2.55.jar accountdb migrate /Signal-Server/service/config/Signal.yml
