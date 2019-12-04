659df19cfb4876a628e6146cdd9488fce5767812
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-04 23:35:25 +0530

    Update migrate-and-start-server.sh

--- migrate-and-start-server.sh
+++ migrate-and-start-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -exu
-java -jar /usr/share/TextSecureServer.jar abusedb migrate /etc/signal-server.yaml
-java -jar /usr/share/TextSecureServer.jar accountdb migrate /etc/signal-server.yaml
-java -jar /usr/share/TextSecureServer.jar messagedb migrate /etc/signal-server.yaml
-java -jar /usr/share/TextSecureServer.jar server /etc/signal-server.yaml
+# java -jar /usr/share/TextSecureServer.jar abusedb migrate /etc/Signal.yml
+java -jar /usr/share/TextSecureServer.jar accountdb migrate /etc/Signal.yml
+java -jar /usr/share/TextSecureServer.jar messagedb migrate /etc/Signal.yml
+java -jar /usr/share/TextSecureServer.jar server /etc/Signal.yml
+java -jar  /usr/share/websocket-resources-2.55.jar
+
