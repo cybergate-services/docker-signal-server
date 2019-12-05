1ab5f3cb30a9aa1b77014d93824a965bb9a6c5de
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-05 12:43:48 +0530

    Update migrate-and-start-server.sh

--- migrate-and-start-server.sh
+++ migrate-and-start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -exu
-java -jar /usr/share/TextSecureServer.jar abusedb migrate /etc/Signal.yml
+# java -jar /usr/share/TextSecureServer.jar abusedb migrate /etc/Signal.yml
 java -jar /usr/share/TextSecureServer.jar accountdb migrate /etc/Signal.yml
 java -jar /usr/share/TextSecureServer.jar messagedb migrate /etc/Signal.yml
 java -jar /usr/share/TextSecureServer.jar server /etc/Signal.yml
